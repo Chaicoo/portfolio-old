0e8adcf2ee18887e95b72cc893a78e756571d082
Francisco <gabriellimma27@gmail.com>
2023-05-21 17:08:34 -0300

    fix: pdf path download

--- src/components/DownloadButton/index.tsx
+++ src/components/DownloadButton/index.tsx
@@ -3,7 +3,6 @@ import { Container } from "./styles";
 export const DownloadButton = () => {
     const handleDownload = () => {
         const link = document.createElement("a");
-        //caminho direto para o pdf
         link.href = "/portifolio/pdf/CV-Francisco.pdf";
         link.download = "curriculo.pdf";
         link.click();

3d06db6b78c21675db7bc1c606e29d94d0a066f9
Francisco <gabriellimma27@gmail.com>
2023-05-23 08:44:27 -0300

    feat: add card component

---
+++ src/pages/Home/components/Card/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Card: React.FC = () => {
+    return (
+        <Container>
+            <h2>Card</h2>
+        </Container>
+    );
+};
+
+export default Card;

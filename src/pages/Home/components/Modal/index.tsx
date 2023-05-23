fc5e43c4302e2a2e550dc050c62efd24f17371e4
Francisco <gabriellimma27@gmail.com>
2023-05-23 08:44:53 -0300

    feat: add modal component

---
+++ src/pages/Home/components/Modal/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Modal: React.FC = () => {
+  return (
+        <Container>
+            <h2>Modal</h2>
+        </Container>
+    );
+}
+
+export default Modal;
